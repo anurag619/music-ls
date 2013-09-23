d3e619db11f8d44b21ee405422bf5efb1ff211ff
Anurag Kumar <anurag3rdsep@gmail.com>
2013-09-24 02:38:26 +0530

    handling errors

--- music.py
+++ music.py
@@ -7,18 +7,25 @@ from google import search
 import sys
 
 def main():
-   fbconsole.AUTH_SCOPE = ['user_likes' ]
+ try:
+  fbconsole.AUTH_SCOPE = ['user_likes' ]	#fbconsole authentication
    fbconsole.authenticate()
    music = fbconsole.fql("SELECT music FROM user  WHERE uid=me()")
    music_names(music)
 
-def music_names(music):
+   except:		#handling HTTP errors
+    print "some error occured in your network, please try again after some time."
+        
+
+   
+
+def music_names(music):		#parsing the music names
    music_list = music[0].values()
    for name in music_list:
     music_name = (choice(name.split(','))).encode('utf-8')
-    for url in search('%s gaana' %music_name, stop=1):
+    for url in search('%s gaana' %music_name, stop=1):		#searching for the names in gaana.com
       if re.search(ur'http://gaana.com', url, re.UNICODE):
-	option = raw_input('now playing %s, N-> for next song; Press Y-> play :' %music_name)
+	option = raw_input('now playing %s, N-> for next song; Press Y-> play :' %music_name) #user options
 	if option == 'N':
 	 music_names(music)
 	else:
