b067e68acffe6b48069d611558f97b3b7d373391
Anurag Kumar <anurag3rdsep@gmail.com>
2013-09-24 02:12:27 +0530

    added user options

--- music.py
+++ music.py
@@ -5,17 +5,24 @@ import re
 import webbrowser
 from google import search
 
-fbconsole.AUTH_SCOPE = ['user_likes' ]
-fbconsole.authenticate()
-q = fbconsole.fql("SELECT music FROM user  WHERE uid=me()")
-s = q[0].values()
-for name in s:
- n = (choice(name.split(','))).encode('utf-8')
- for url in search('%s gaana' %n, stop=1):
+def main():
+   fbconsole.AUTH_SCOPE = ['user_likes' ]
+   fbconsole.authenticate()
+   music = fbconsole.fql("SELECT music FROM user  WHERE uid=me()")
+   music_names(music)
+
+def music_names(music):
+   music_list = music[0].values()
+   for name in s:
+    music_name = (choice(name.split(','))).encode('utf-8')
+    for url in search('%s gaana' %music_name, stop=1):
       if re.search(ur'http://gaana.com', url, re.UNICODE):
 	webbrowser.open_new_tab(url)
-	break
- break
+	
+
+if __name__=='__main__':
+ main()
+ sys.exit(0)
  
 
 
