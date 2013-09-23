70590e09d4cf95de89851cfb541c88573fa08ab6
Anurag Kumar <anurag3rdsep@gmail.com>
2013-09-24 02:13:46 +0530

    added some user options

--- music.py
+++ music.py
@@ -4,6 +4,7 @@ from random import choice
 import re
 import webbrowser
 from google import search
+import sys
 
 def main():
    fbconsole.AUTH_SCOPE = ['user_likes' ]
@@ -13,11 +14,19 @@ def main():
 
 def music_names(music):
    music_list = music[0].values()
-   for name in s:
+   for name in music_list:
     music_name = (choice(name.split(','))).encode('utf-8')
     for url in search('%s gaana' %music_name, stop=1):
       if re.search(ur'http://gaana.com', url, re.UNICODE):
-	webbrowser.open_new_tab(url)
+	option = raw_input('now playing %s, N-> for next song; Press Y-> play :' %music_name)
+	if option == 'N':
+	 music_names(music)
+	else:
+	 webbrowser.open_new_tab(url)
+      else:
+        print "your like doesn't match any song. N-> for next song: "
+	break
+    break
 	
 
 if __name__=='__main__':
