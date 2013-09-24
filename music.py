239516ef88977feb051f4d43c6dbabbdc19dce78
Anurag Kumar <anurag3rdsep@gmail.com>
2013-09-24 09:42:57 +0530

    removed http errors

--- music.py
+++ music.py
@@ -6,37 +6,39 @@ import webbrowser
 from google import search
 import sys
 
-
 def main():
  try:
-  fbconsole.AUTH_SCOPE = ['user_likes' ]	#fbconsole authentication
+   fbconsole.AUTH_SCOPE = ['user_likes','publish_checkins' ]
    fbconsole.authenticate()
    music = fbconsole.fql("SELECT music FROM user  WHERE uid=me()")
    music_names(music)
 
-   except:		#handling HTTP errors
+ except:
     print "some error occured in your network, please try again after some time."
         
 
-def music_names(music):		#parsing the music names
+def music_names(music):
    music_list = music[0].values()
    for name in music_list:
     music_name = (choice(name.split(','))).encode('utf-8')
-    for url in search('%s gaana' %music_name, stop=1):		#searching for the names in gaana.com
+    for url in search('%s gaana' %music_name, stop=1):
       if re.search(ur'http://gaana.com', url, re.UNICODE):
-	option = raw_input('now playing %s, N-> for next song; Press Y-> play :' %music_name) #user options
+	option = raw_input('now playing %s, N-> for next song; Press Y-> play :' %music_name)
 	if option == 'N':
 	 music_names(music)
 	else:
 	 webbrowser.open_new_tab(url)
       else:
-        print "your like doesn't match any song. N-> for next song: "
-	break
-    break
+        option1= raw_input( "your like doesn't match any song. N-> for next song: ")
+	if option1 == 'N':
+	   music_names(music)
 	
 
 if __name__=='__main__':
  main()
  sys.exit(0)
+ 
+
+
 
 
