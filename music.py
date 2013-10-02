7c679453215897b78dbea1f3f20527bcddac2c2e
anurag kumar <anurag3rdsep@gmail.com>
2013-10-02 23:47:30 +0530

    removed errors

--- music.py
+++ music.py
@@ -13,41 +13,46 @@ def main():
     print "do you wish to add some songs yourself or listen according to your likes?"
     choice = raw_input("A--> to add songs \nL--> listen according to your likes. \nEnter your choice:  ")
     if choice == 'A':
-     user_input_name = input("Enter your song/artist: ").split(',')     
-     for name in user_input_name:
-      print "please wait while we search for your entries"
-      song_name = google_search(name)
-      if song_name!='nothin':
-       print "opening gaana to play your songs.Enjoy!"
-       time.sleep(1)
-       driver = webdriver.Firefox()
-       driver.get("http://gaana.com")
-       driver.find_element_by_id("keywordsearch").clear()
-       driver.find_element_by_id("keywordsearch").send_keys(name)
-       driver.find_element_by_id("btnsearch").click()
-       time.sleep(2)
-       driver.find_element_by_css_selector("#tblheader1 > tbody > tr.GridV1BG > th[name=\"checkSelected\"] > div > input[name=\"checkSelected\"]").click()
-       time.sleep(1)
-       driver.find_element_by_link_text("Add to Queue").click()
-
-     driver.find_element_by_link_text("Now Playing").click()
-     driver.find_element_by_css_selector("div.checkbx > input[name=\"chk\"]").click()
-     driver.find_element_by_xpath("//div[@id='mainPlayer']/div[3]/div[2]/div[6]/a").click()
-    
-
+        user_input()
     elif choice == 'L':
-     fb_access()
+        fb_access()
     else:
-     print "wrong option, please try again."
-     main()
+        print "wrong option, please try again."
+        main()
+
+def user_input():
+    user_input_name = input("Enter your song/artist: ").split(',')    
+    driver = webdriver.Firefox()
+    driver.get("http://gaana.com") 
+    for name in user_input_name:
+        song_name = google_search(name)
+        if song_name!='nothing':
+            driver.find_element_by_id("keywordsearch").clear()
+            driver.find_element_by_id("keywordsearch").send_keys(name)
+            driver.find_element_by_id("btnsearch").click()
+            time.sleep(2)
+            driver.find_element_by_css_selector("#tblheader1 > tbody > tr.GridV1BG > th[name=\"checkSelected\"] > div > input[name=\"checkSelected\"]").click()
+            time.sleep(1)
+            driver.find_element_by_link_text("Add to Queue").click()
+    
+        else:
+            print "your option is not present at gaana \n please try again."
+            user_input()
+
+    driver.find_element_by_link_text("Now Playing").click()
+    driver.find_element_by_css_selector("div.checkbx > input[name=\"chk\"]").click()
+    driver.find_element_by_xpath("//div[@id='mainPlayer']/div[3]/div[2]/div[6]/a").click()
+
+    
+    
 
 def google_search(name):
     print 'searching...'
     for url in search('%s gaana' %name, stop=1):
-     if re.search(ur'http://gaana.com', url, re.UNICODE):
-      return name 
-     else:
-      return 'nothing'
+        if re.search(ur'http://gaana.com', url, re.UNICODE):
+            return name 
+        else:
+            return 'nothing'
 
 
 def fb_access():
@@ -66,29 +71,42 @@ def music_names(music):
  for a valid url, which is then feeded into gaana.com"""
     music_list = music[0].values()
     for name in music_list:	#fetch music names
-     music_name = (choice(name.split(','))).encode('utf-8')
-     search = google_search(music_name)
-     print "please wait while we search for your entries.."
-     if search!= 'nothing':
-       option = raw_input('now playing %s, N-> for next song; Press Y-> play; Q--> quit :' %search) #user options
-       if option == 'N':
-        music_names(music)
-       elif option == 'Y':
-        webdriver.Firefox().get(url)     #evoking gaana.com
-       elif option =='Q':
-        sys.exit(0)		
-       else:
-         print "wrong option,please try again."
-         music_names(music)		
-     else:
-       option= raw_input( "your like doesn't match any song. N-> for next song; Q--> quit: ")
-       if option == 'N':
-        music_names(music)
-       elif option == 'Q':
-	sys.exit(0)
-       else: 
-        print "wrong option,please try again."
-        music_names(music)
+        music_name = (choice(name.split(','))).encode('utf-8')
+        search = google_search(music_name)
+        print "please wait while we search for your entries.."
+        if search!= 'nothing':
+            option = raw_input('now playing %s, N-> for next song; Press Y-> play; Q--> quit :' %search) #user options
+            if option == 'N':
+                music_names(music)
+            elif option == 'Y':
+                time.sleep(1)
+                driver = webdriver.Firefox()
+                driver.get("http://gaana.com")
+                driver.find_element_by_id("keywordsearch").clear()
+                driver.find_element_by_id("keywordsearch").send_keys(search)
+                driver.find_element_by_id("btnsearch").click()
+                time.sleep(2)
+                driver.find_element_by_css_selector("#tblheader1 > tbody > tr.GridV1BG > th[name=\"checkSelected\"] > div > input[name=\"checkSelected\"]").click()
+                time.sleep(1)
+                driver.find_element_by_link_text("Add to Queue").click()
+     
+                driver.find_element_by_link_text("Now Playing").click()
+                driver.find_element_by_css_selector("div.checkbx > input[name=\"chk\"]").click()
+                driver.find_element_by_xpath("//div[@id='mainPlayer']/div[3]/div[2]/div[6]/a").click()
+            elif option =='Q':
+                sys.exit(0)		
+            else:
+                print "wrong option,please try again."
+                music_names(music)		
+        else:
+            option= raw_input( "your like doesn't match any song. N-> for next song; Q--> quit: ")
+            if option == 'N':
+                music_names(music)
+            elif option == 'Q':
+	        sys.exit(0)
+            else: 
+                 print "wrong option,please try again."
+                 music_names(music)
           
 
 if __name__=='__main__':
